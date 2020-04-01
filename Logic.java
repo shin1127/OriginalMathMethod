49701f4feb830f5868f6c9bbe65538b07fb7eaf9
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-04-01 17:25:51 +0900

    2つのaverageメソッドにおいて、不具合を含む
    名前が競合しています。average(java.util.ArrayList<java.lang.Double>)とaverage(java.util.ArrayList<java.lang.Integer>)は削除後の名前が同じです
    Logic.java:80
    
    参考URL：https://hacknote.jp/archives/17792/

--- Logic.java
+++ Logic.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Logic{
 
-	
+
 	// int型の配列の最大値を求める
     public int maxInt(ArrayList<Integer> list){
         int maxNumber = Integer.MIN_VALUE;
@@ -15,7 +15,7 @@ public class Logic{
         }
         return maxNumber;
     }
-    
+
     // double型の配列の最大値を求める
     public double maxDouble(ArrayList<Double> list){
         double maxNumber = Double.MIN_VALUE;
@@ -26,7 +26,7 @@ public class Logic{
         }
         return maxNumber;
     }
-    
+
     // int型の配列の最小値を求める
     public int minInt(ArrayList<Integer> list){
         int minNumber = Integer.MAX_VALUE;
@@ -37,7 +37,7 @@ public class Logic{
         }
         return minNumber;
     }
-    
+
     // double型の配列の最小値を求める
     public double minDouble(ArrayList<Double> list){
         double minNumber = Double.MIN_VALUE;
@@ -48,4 +48,45 @@ public class Logic{
         }
         return minNumber;
     }
+
+    // int型の配列の和を求める
+    public int sumInt(ArrayList<Integer> list) {
+    	int sumNumber = 0;
+    	for(int i : list) {
+    		sumNumber += i;
+    	}
+    	return sumNumber;
+    }
+
+    // double型の配列の和を求める
+    public double sumDouble(ArrayList<Double> list) {
+    	double sumNumber = 0;
+    	for(double d : list) {
+    		sumNumber += d;
+    	}
+    	return sumNumber;
+    }
+
+    
+    public double average(ArrayList<Integer> list) {
+    	int sumNumber = 0;
+    	double avgNumber = 0;
+    	for(int i : list) {
+    		sumNumber += i;
+    	}
+    	avgNumber = sumNumber / list.size();
+    	return avgNumber;
+    }
+
+    // int型の
+    public double average(ArrayList<Double> list) {
+    	double sumNumber = 0;
+    	double avgNumber = 0;
+    	for (double d : list) {
+    		sumNumber += d;
+    	}
+    	avgNumber = sumNumber / list.size();
+    	return avgNumber;
+    }
+
 }
